db1c2b4253c09a1ac2880aef2cdfbfd3a01e0bce
Guiaochino <chinotiamzon@gmail.com>
2021-01-28 14:02:28 +0800

    Finished Probuilder Update

---
+++ Assets/Scripts/GameEnder.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameEnder : MonoBehaviour
+{
+    public Text text;
+    private bool won = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        text.color = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump") && won)
+        {
+            SceneManager.LoadScene("Level");
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        text.color = new Color(0, 0, 0, 1);
+        text.text = "You Won!\nPress \"Space\" to Restart\nthe Game!";
+        won = true;
+    }
+}
